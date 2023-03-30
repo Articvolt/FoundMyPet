854c00a92f0dd0992cc3a9e24252b3d5283a354f
Articvolt <ugodasilva67@gmail.com>
2023-03-30 11:34:08 +0200

    NOT FINISHED : add form + controller to message ( create a commentary section)

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -124,4 +124,6 @@ class AnnonceController extends AbstractController
             'edit' => $annonce->getId()
         ]);
     }
+
+    
 }
---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'valider'
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
