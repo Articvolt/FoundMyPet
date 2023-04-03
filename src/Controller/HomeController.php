c975762389e755b3c1f6dfe693e64c46a5f8da31
Articvolt <ugodasilva67@gmail.com>
2023-04-03 09:06:41 +0200

    add paginator to homepage + add param to use this

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Membre;
 use App\Repository\AnnonceRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +15,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(AnnonceRepository $annonceRepository, Request $request , PaginatorInterface $paginatorInterface): Response
     {
-        $annonces = $annonceRepository->findAll();
+        $data = $annonceRepository->findAll();
+        $annonces = $paginatorInterface->paginate(
+            $data,
+            $request->query->getInt('page', 1), 3
+        );
 
         return $this->render('home/index.html.twig', [
             'annonces' => $annonces,
